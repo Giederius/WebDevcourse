3c7a45b89846739cd2f2f74b295f071e08d19584
Giedrius Mėčius <giedriusmecius@gmail.com>
2021-01-02 10:05:48 +0100

    Drum kit completed

--- 5.Drum-Kit/index.js
+++ 5.Drum-Kit/index.js
@@ -1,1 +1,67 @@
-addEventListener.
+// finds out number of buttons in the class drum
+var drumNumber = document.querySelectorAll(".drum").length;
+
+// detecting button press
+for (i = 0; i < drumNumber; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+// detecting keyboard press
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    case "s":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+
+    case "d":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+
+    case "j":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+
+    case "k":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+
+    case "l":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+
+    // default;
+  }
+}
+// tiek is keyboard press, tiek is button press pasiimimas ta pati key value,
+// kuria veliau "pernesa" i sita funkcija.
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  //   adding pressed class to the activeButton
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
