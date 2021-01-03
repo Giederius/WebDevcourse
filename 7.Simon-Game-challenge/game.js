393462485264977b3982116c2caac13f877351fa
Giedrius Mėčius <giedriusmecius@gmail.com>
2021-01-03 12:15:53 +0100

    game started to work as planned

--- 7.Simon-Game-challenge/game.js
+++ 7.Simon-Game-challenge/game.js
@@ -5,18 +5,56 @@ var userClickedPattern = [];
 // button colors
 var buttonColors = ["red", "blue", "green", "yellow"];
 
+var started = false;
+
+var level = 0;
+
+// GAME START
+// detecting button press for the start of the game
+document.addEventListener("keydown", function (e) {
+  var startButton = e.keyCode;
+  if (startButton === 65 && !started) {
+    document.querySelector("h1").innerHTML = "Level " + level;
+    nextSequence();
+    started = true;
+  }
+});
+
 function nextSequence() {
+  userClickedPattern = [];
+  //   keeping track of what level we are in
+  level++;
+
+  //   changing the h1 to reflect which level we are in
+  document.querySelector("h1").innerHTML = "Level " + level;
   // function to generate random number between 0 and 3
   var randomNumber = Math.floor(Math.random() * 4);
+
   // randomizes which color is picked
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  console.log(randomChosenColor);
 
   flashAnimation(randomChosenColor);
 
   playSound(randomChosenColor);
 }
+// Checking answers
+function checkAnswer(currentLevel) {
+  console.log(currentLevel);
+  console.log(gamePattern);
+
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
+}
+
 // Detecting user button press
 var buttonNumbers = document.querySelectorAll(".btn").length;
 
@@ -26,6 +64,7 @@ for (i = 0; i < buttonNumbers; i++) {
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
   });
 }
 
@@ -33,7 +72,6 @@ for (i = 0; i < buttonNumbers; i++) {
 
 function flashAnimation(randomChosenColor) {
   var activeButton = document.querySelector("#" + randomChosenColor);
-  console.log(activeButton);
   activeButton.classList.add("played");
   setTimeout(function () {
     activeButton.classList.remove("played");
@@ -43,7 +81,6 @@ function flashAnimation(randomChosenColor) {
 // User press animation
 function animatePress(currentColor) {
   var activeButton = document.querySelector("#" + currentColor);
-  console.log(activeButton);
   activeButton.classList.add("pressed");
   setTimeout(function () {
     activeButton.classList.remove("pressed");
