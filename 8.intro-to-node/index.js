7de70d6dc48c65bec92818177b755c60f2aef996
Giedrius Mėčius <giedriusmecius@gmail.com>
2021-01-04 10:43:38 +0100

    introduction to node

---
+++ 8.intro-to-node/index.js
@@ -0,0 +1,7 @@
+var superHeros = require("superheroes");
+var supervillains = require("supervillains");
+
+var mysuperheroName = superHeros.random();
+var mysupervillainName = supervillains.random();
+console.log(mysuperheroName);
+console.log(mysupervillainName);
